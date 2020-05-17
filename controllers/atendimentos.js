7dc0df14520b623f86ff54ee99c8eb34865d8102
Leonardo Croda <lccroda@gmail.com>
2020-05-17 02:54:53 -0300

    conectando ao mysql e criando tabela atendimentos

--- controllers/atendimentos.js
+++ controllers/atendimentos.js
@@ -1,7 +1,11 @@
+const Atendimento = require('../models/atendimentos');
+
 module.exports = app => {
     app.get('/atendimentos', (req, res) => res.send('Voce está na rota de atendimentos'));
 
     app.post('/atendimentos', (req, res) => {
-        res.send("vc esta na rota de atendimentos e está realizando um POST");
+        const atendimento = req.body;
+        Atendimento.adiciona(atendimento);
+        res.send("ok");
     })
 }
